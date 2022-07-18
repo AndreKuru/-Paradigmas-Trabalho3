312c9ea15ab53f04cc0d5f4e30c052de845e57c0
julien-vaz <54545601+julien-vaz@users.noreply.github.com>
2022-07-17 22:02:21 -0300

    Update and rename trabalho3.pl to Matrix4.pl
    Puzzle de ordem 4 funcionando

---
+++ Matrix4.pl
@@ -0,0 +1,34 @@
+:- use_module(library(clpfd)).
+
+:- use_rendering(table).
+
+solucao(4, Answer) :-
+    
+    Row1 = [X11, X12, X13, X14],
+    Row2 = [X21, X22, X23, X24],
+    Row3 = [X31, X32, X33, X34],
+    Row4 = [X41, X42, X43, X44],
+    Rows = [Row1, Row2, Row3, Row4],
+    append(Rows, Elements), Elements ins 1..4,
+    maplist(all_distinct, Rows),
+  
+    transpose(Rows, Columns),
+    maplist(all_distinct, Columns),
+    
+    blocks(Row1,Row2), blocks(Row3,Row4),
+    
+    %seção de operadores (para mudar o puzzle, mudar apenas os operadores aqui)
+    X11 #< X12,              X13 #< X14,
+    X21 #< X11, X22 #> X12,  X23 #> X13, X24 #< X14,
+    X21 #< X22,              X23 #> X24,
+    
+    X31 #> X32,              X33 #< X34,
+    X41 #< X31, X42 #> X32,  X43 #> X33, X44 #< X34,
+    X41 #> X42,              X43 #> X44,
+
+  	Answer = Rows.
+
+blocks([], []).
+blocks([A,B|Rows1], [C,D|Rows2]) :-
+    all_distinct([A,B,C,D]),
+    blocks(Rows1, Rows2).
